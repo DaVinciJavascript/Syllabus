64e82d7d51c8e6d4ef7f13c5fd892486cfc0dc57
Bishop Zareh <xchicago@gmail.com>
2016-08-17 15:03:08 -0600

    adding new project template

---
+++ week1/Class-Template-1/server/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.set('views', __dirname + '/');
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', routes);
+
+// error handlers
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development prints a stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+
+// production, no stacktrace
+} else {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
+}
+
+// Start the server
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+

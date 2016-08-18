c11cec0856130f212dd8d6978a8177e40cca558b
Bishop Zareh <xchicago@gmail.com>
2016-08-18 12:38:27 -0600

    added template library

--- week1/Class-Template-2/app/app.js
+++ week1/Class-Template-2/app/app.js
@@ -2,17 +2,17 @@
 // base framework
 import $ from 'jquery';
 
-// legacy loading for bootstrap js
+// legacy loading for bootstrap javascript
 window.$ = window.jQuery = $;
 require('bootstrap');
 
 // import our styles
 import './stylesheets/base.scss';
+import _ from 'underscore';
 
 // some extras
 // import { TweenLite } from 'gsap';
-// import _ from 'underscore';
-
+// import Backbone from 'backbone';
 
 // on document load
 $(function(){
@@ -22,7 +22,22 @@ $(function(){
   // add some html content
   $('#root').html('<button class="btn btn-default" type="submit">Button</button>');
 
+  // use a template
+  _.templateSettings = {
+    evaluate:    /{{([\s\S]+?)}}/g,
+    interpolate: /{{-([\s\S]+?)}}/g,
+    escape:      /{{=([\s\S]+?)}}/g
+  };
+
+  var template = '<button class="btn btn-default" type="submit">{{= name}}</button>';
+  var compiledTemplate = _.template(template);
+  $('#root').html(compiledTemplate({name: '<fruit>'}));
+
   // optionally animate that content
   // TweenLite.to($('#root'), 1, {fontSize: 50}).play();
+  
+  // we can use Backbone too
+  // var view = Backbone.View.extend({});
+  // console.log(view);
 
 });
